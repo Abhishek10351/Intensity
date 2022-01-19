2d64fe217c2003be0c6f0522d51d54258d5cc9cf
Abhishek Kashyap <abhik5567@gmail.com>
2022-01-19 13:14:16 +0530

    Added a small info command

--- My_bot/cogs/misc.py
+++ My_bot/cogs/misc.py
@@ -4,9 +4,9 @@ import time
 from sql import fetch
 from timeit import timeit
 from mongo import prefixes
+from buttons import Paginator
 
-
-class misc(commands.Cog):
+class Misc(commands.Cog):
 
     def __init__(self, Intensity):
         self.Intensity = Intensity
@@ -27,24 +27,45 @@ class misc(commands.Cog):
 
         @timer
         def sqldb():
-            fetch(f'select * from muted_roles where guild_id = {ctx.guild.id}')
+            fetch(f'select * from muted_roles where guild_ID = {ctx.author.id}')
 
         @timer
         def mongodb():
-            prefixes.find_one({"_id": ctx.guild.id})
+            prefixes.find_one({"_id": ctx.author.id})
 
         embed = disnake.Embed(description='**Ping**',
-                              colour=disnake.Colour.red())
-        embed.add_field(name='Discord Websocket Latency <:discord:915501670979477524>',
-                        value=f'`{round((self.Intensity.latency) * 1000)} ms`')
+                              colour=disnake.Colour.random())
+        embed.add_field(name='Discord Websocket <:discord:915501670979477524>',
+                        value=f'`{round((self.Intensity.latency) * 1000)} ms`', inline=False)
         embed.add_field(
-            name='Typing Latency <a:typing:883235601187487824>', value=f'`{round(k*1000)} ms`')
+            name='Typing <a:typing:883235601187487824>', value=f'`{round(k*1000)} ms`', inline=False)
         embed.add_field(name='Sqlite3 Latency <:sqlite3:879576089238831145>',
-                        value=f'`{round(sqldb()*1000)} ms`')
-        embed.add_field(name="MongoDB Latency <:mongo_db:915501188978454568>",
-                        value=f"`{round(mongodb()*1000)} ms`", inline=True)
+                        value=f'`{round(sqldb()*1000)+1} ms`', inline=False)
+        embed.add_field(name="MongoDB <:mongo_db:915501188978454568>",
+                        value=f"`{round(mongodb()*1000)} ms`", inline=False)
         await a.edit(content='', embed=embed)
+    
+    @commands.command()
+    @commands.cooldown(1, 60, commands.BucketType.channel)
+    async def info(self, ctx):
+        class Invite(disnake.ui.View):
+            def __init__(self):
+                super().__init__()
+                self.add_item(disnake.ui.Button(label="Invite me",  url="https://discord.com/api/oauth2/authorize?client_id=852203850214801428&permissions=1394723912950&scope=bot"))
+        embed = disnake.Embed(description="**Hey, I am Intensity a multi-purpose discord bot with many features**", colour=disnake.Colour.random())
+        embed.add_field("Creator", "Peter parker#1716")
+        embed.add_field("Library", "Disnake")
+        embed.add_field("Server count", str(len(self.Intensity.guilds)))
+        embed.add_field("User Count", str(len(self.Intensity.users)))
+        await ctx.send(embed=embed, view=Invite())
+    
+    @commands.command(name="test", is_hidden=True)
+    async def test(self, ctx):
+        view = Paginator()
+        await ctx.send(content=0, view=view)
+    
+    
 
 
 def setup(Intensity):
-    Intensity.add_cog(misc(Intensity))
+    Intensity.add_cog(Misc(Intensity))
